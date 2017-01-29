b60ae399e82cf128ec44f09aec88119089fa290e
Wouter R <gennerate@zonnet.nl>
2017-01-29 04:08:39 +0100

    Further reordering/cleanup of node render code.

--- src/renderer/containers/nodes/node.js
+++ src/renderer/containers/nodes/node.js
@@ -46,12 +46,9 @@ class Node extends React.Component {
     const bgImagePath = path.join(IMG_PATH, 'white.png'); //this 'solves' the problem of the render order (makes sure each frame is rendered before each image) but is not really elegant
     const ringImagePath = path.join(IMG_PATH, 'red.png');
     const imagePath = path.join(IMG_PATH, this.props.imageName);
-    const ringMaterial = THREE.meshBasicMaterial;
-    const ringColor = new THREE.Color();
     const w = this.props.size.x;
     const h = this.props.size.y;
     const frameMargin = 1 + FRAME_MARGIN;
-    const theID = this.props.id;
 
     // TODO: (perhaps) share as many things under resources and share those among all nodes to save memory usage
     return (<group position={this.props.position}>
@@ -72,17 +69,32 @@ class Node extends React.Component {
           }}
         />
 
-        <meshBasicMaterial resourceId="bgMat" color={0xFFFFFF} />
-        <meshBasicMaterial resourceId="bgImageMat" color={0xF0F0F0} depthTest={false} depthWrite={false}>
-          {/* <textureResource resourceId="bgImageTex" /> */}
+        <texture
+          resourceId="ringImageTex"
+          url={ringImagePath}
+          onError={() => {
+            console.error(`could not load ring image ('${ringImagePath}')`);
+          }}
+        />
+
+        {/* <meshBasicMaterial resourceId="bgMat" color={0xFFFFFF} /> */}
+        {/* <meshBasicMaterial resourceId="bgImageMat" color={0xF0F0F0} depthTest={false} depthWrite={false}> */}
+        <meshBasicMaterial resourceId="bgImageMat" color={0xFFFFFF}>
+          <textureResource resourceId="bgImageTex" />
         </meshBasicMaterial>
-        <meshBasicMaterial resourceId="imageMat" color={0x00FFFF} depthTest={false} depthWrite={false}>
+
+        {/* <meshBasicMaterial resourceId="imageMat" color={0x00FFFF} depthTest={false} depthWrite={false}> */}
+        <meshBasicMaterial resourceId="imageMat" color={0xFFFFFF}>
           <textureResource resourceId="imageTex" />
         </meshBasicMaterial>
-        <meshBasicMaterial resourceId="ringMat" color={this.props.color} depthTest={false} depthWrite={false} />
+
+        {/* <meshBasicMaterial resourceId="ringMat" color={this.props.color} /> */}
+        {/* <meshBasicMaterial resourceId="ringMat" color={this.props.color} depthTest={false} depthWrite={false}> */}
+        <meshBasicMaterial resourceId="ringImageMat">
+          <textureResource resourceId="ringImageTex" />
+        </meshBasicMaterial>
       </resources>
 
-      {/* this is the background plane */}
       <mesh position= {new THREE.Vector3(0, 0, 0)}>
         <planeBufferGeometry name="bgGeom" width={w * frameMargin} height={h * frameMargin}></planeBufferGeometry>
         <materialResource resourceId="bgImageMat" />
@@ -93,30 +105,14 @@ class Node extends React.Component {
         <materialResource resourceId="imageMat" />
       </mesh>
 
-      <resources>
-        <meshBasicMaterial resourceId="ringMat" color={0xFFFFFF} />
-        <texture
-          resourceId="ringImageTex"
-          url={ringImagePath}
-        />
-        <meshBasicMaterial resourceId="ringImageMat">
-          <textureResource resourceId="ringImageTex" />
-        </meshBasicMaterial>
-      </resources>
-      <mesh
-        position={new THREE.Vector3(NODE_WIDTH * 0.5 + RING_RADIUS * 2, 0, 0)}
-      >
+      <mesh position={new THREE.Vector3(NODE_WIDTH * 0.5 + RING_RADIUS * 2, 0, 0)}>
         {/* FIXME: should be ringBufferGeometry but that is currently unimplemented (see: https://github.com/toxicFork/react-three-renderer/issues/2#issuecomment-250013560) */}
-
-
         <ringGeometry name="ringGeom"
           innerRadius={RING_RADIUS}
           outerRadius={RING_RADIUS + RING_THICKNESS}
           thetaSegments={32}
-        >
-        </ringGeometry>
-
-         <materialResource resourceId="ringImageMat" />
+        />
+        <materialResource resourceId="ringImageMat" />
       </mesh>
     </group>);
   }
